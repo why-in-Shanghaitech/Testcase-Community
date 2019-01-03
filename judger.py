a4c695d9cd1888923995577a3803a96aa3469fe9
why-in-Shanghaitech <43395692+why-in-Shanghaitech@users.noreply.github.com>
2019-01-03 21:08:48 +0800

    V0.0.1 judger update
    解决一个小BUG并以牺牲一定运行时间为代价增加可视化效果。

--- judger.py
+++ judger.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import time
+import sys
+import threading
 
 # fit the environment
 time_limit = 0.1        # default time limit is 0.1 s
@@ -12,7 +14,7 @@ if os.name=='nt':
     python_version = "python"
     time_limit = 1      # default time limit on windows is 1 s
 elif os.name=='posix':
-	# if it is under linux
+    # if it is under linux
     pass
 
 # update testcases
@@ -27,14 +29,24 @@ os.system("git fetch --all && git reset --hard origin/master")
 i = 1
 case = []
 ac_case, wa_case, re_case, tle_case = 0, 0, 0, 0
-st_dict = {'ac ':'Accept',
-            'wa ':'Wrong Answer',
-            're ':'Run Time Error',
-            'tle':'Time Limit Exceeded'}
+st_dict = {'ac ':('\033[32m','Accept','\033[0m'),
+            'wa ':('\033[31m','Wrong Answer','\033[0m'),
+            're ':('\033[33m','Run Time Error','\033[0m'),
+            'tle':('\033[34m','Time Limit Exceeded','\033[0m')}
 
 # check the input file
 if not(os.path.exists(pre + "hw6.py")):
     pass
+
+# function of showing the progress
+def ProgShow():
+    prog_state = ['|','\\','–','/']
+    while Continue_Showing:   
+        print("\rJudging: testcase {0} is being judged...{1}".format(i, prog_state[int(time.time()*8)%4]), end='')
+        sys.stdout.flush()
+
+Showing_the_program, Continue_Showing= threading.Thread(target=ProgShow), True
+Showing_the_program.start()
 
 while True:
     # check the input file
@@ -78,18 +90,20 @@ while True:
             break
     i += 1
 
-	
+# Stop showing the progress
+Continue_Showing = False
+Showing_the_program.join()
 
 # print the report
 print("\n\n\n\n========= Below is your report =========")
 print("    ID           Status         Time\n")
 for i in range(len(case)):
-    print("   {0:3}     {1:^19}  {2:.2f}ms".format(i+1,st_dict[case[i][0]],1000*case[i][1]))
+    print("   {0:3}     {3}{1:^19}{4}  {2:.2f}ms".format(i+1, st_dict[case[i][0]][1], 1000*case[i][1], st_dict[case[i][0]][0], st_dict[case[i][0]][2]))
 print("\n=========== The final result ===========")
 print("          Status            Numbers\n")
 static = [ac_case, wa_case, re_case, tle_case]
 status = ['ac ','wa ','re ','tle']
 for i in range(4):
-    print("    {0:^19}       {1:^3}".format(st_dict[status[i]],static[i]))
+    print("    {2}{0:^19}{3}       {1:^3}".format(st_dict[status[i]][1], static[i], st_dict[status[i]][0], st_dict[status[i]][2]))
 print("----------------------------------------")
 print("    {0:^19}       {1:^3}".format('Total',sum(static)))
